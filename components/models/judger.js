a273e77f81f4ca236bb9839c0034ed978d9a47ae
F-Kapok <5219824@qq.com>
2019-12-26 17:44:04 +0800

    sku完善

--- components/models/judger.js
+++ components/models/judger.js
@@ -1,15 +1,21 @@
 import { SkuCode } from "./sku-code";
 import { CellStatus } from "../../core/enum";
+import { SkuPending } from "./sku-pending";
+import { Joiner } from "../../utils/joiner";
 
 class Judger {
     fenceGroup;
     pathDict = [];
-
+    skuPending;
     constructor(fenceGroup) {
         this.fenceGroup = fenceGroup;
+        this._initSkuPending();
         this._initPathDict();
     }
 
+    _initSkuPending() {
+        this.skuPending = new SkuPending();
+    }
     _initPathDict() {
         this.fenceGroup.spu.sku_list.forEach(s => {
             const skuCode = new SkuCode(s.code);
@@ -18,16 +24,62 @@ class Judger {
         console.log(this.pathDict);
     }
 
-    judge(cell, x, y){
-            this._changeCellStatus(cell, x, y);
+    judge(cell, x, y) {
+        this._changeCurrentCellStatus(cell, x, y);
+        this.fenceGroup.eachCell((cell, x, y) => {
+            const path = this._findPotentailPath(cell, x, y);
+            // console.log(path);
+            if (!path) {
+                return;
+            }
+            const isIn = this._isInDict(path);
+            if (isIn) {
+                this.fenceGroup.fences[x].cells[y].status = CellStatus.WAITING;
+            } else {
+                this.fenceGroup.fences[x].cells[y].status = CellStatus.FORBIDDEN;
+            }
+        });
     }
 
-    _changeCellStatus(cell, x, y){
-        if(cell.status===CellStatus.WAITING){
-            this.fenceGroup.fences[x].cells[y].status = CellStatus.SELECTED;
+    _isInDict(path) {
+        return this.pathDict.includes(path);
+    }
+
+    _findPotentailPath(cell, x, y) {
+        const joiner = new Joiner('#');
+        for (let i = 0; i < this.fenceGroup.fences.length; i++) {
+            const selected = this.skuPending.findSelectedCellByX(i);
+            if (x === i) {
+                //已选行
+                if (this.skuPending.isSelected(cell, x)) {
+                    return;
+                }
+                const cellCode = this._getCellCode(cell.spec);
+                joiner.join(cellCode);
+            } else {
+                //其他行
+                if (selected) {
+                    //是否有已选元素 有的话则加入
+                    const selectedCellCode = this._getCellCode(selected.spec);
+                    joiner.join(selectedCellCode);
+                }
+            }
         }
-        if(cell.status === CellStatus.SELECTED){
+        return joiner.getStr();
+    }
+
+    _getCellCode(spec) {
+        return spec.key_id + '-' + spec.value_id;
+    }
+
+    _changeCurrentCellStatus(cell, x, y) {
+        if (cell.status === CellStatus.WAITING) {
+            this.fenceGroup.fences[x].cells[y].status = CellStatus.SELECTED;
+            this.skuPending.insertCell(cell, x);
+        }
+        if (cell.status === CellStatus.SELECTED) {
             this.fenceGroup.fences[x].cells[y].status = CellStatus.WAITING;
+            this.skuPending.removeCell(x);
         }
     }
 }
