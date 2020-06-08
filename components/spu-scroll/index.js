bee023989b9627cdcc5856a768a89f5df02c589a
F-Kapok <5219824@qq.com>
2020-06-09 01:00:54 +0800

    搜索页面优化

--- components/spu-scroll/index.js
+++ components/spu-scroll/index.js
@@ -1,8 +1,10 @@
 // components/spu-scroll/index.js
+import {Theme} from "../../models/theme";
 Component({
     /**
      * 组件的属性列表
      */
+    externalClasses:['l-class'],
     properties: {
         theme: Object,
         spuList: Array
@@ -19,6 +21,17 @@ Component({
      * 组件的方法列表
      */
     methods: {
-
+        onMore(event) {
+            console.log(event)
+            wx.navigateTo({
+                url: `/pages/theme-spu-list/theme-spu-list?tname=${Theme.locationE}`
+            })
+        },
+        onTap(event) {
+            const spuId = event.currentTarget.dataset.spuId
+            wx.navigateTo({
+                url: `/pages/detail/detail?pid=${spuId}`
+            })
+        },
     }
 })
--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {
-    appkey: 'SHlLO6F3oR7WfMWB',
+    appkey: 'Xwiwuh6gOohvnplX',
     apiBaseUrl: 'http://se.7yue.pro/v1'
+    // apiBaseUrl: 'http://localhost:8081/v1'
 }
 
 export {
