84243087cf830feb3afaabc0eb2cdc9264a72e6c
F-Kapok <5219824@qq.com>
2020-06-24 13:23:49 +0800

    update

---
+++ components/empty-screen/index.js
@@ -0,0 +1,30 @@
+// components/empty-screen/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    text: {
+      type: String,
+      value: '暂无相关商品'
+    },
+    show: {
+      type: Boolean,
+      value: false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
